1ce9ad567797059fb2bf181b3dbe41d5f71b6193
acrispin06 <alvarocrispin0604@gmail.com>
2025-05-14 01:56:04 -0500

    feat: added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material/material.module';
+
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { TranslationModule } from './translation/translation.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { TranslationModule } from './translation/translation.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    TranslationModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatToolbar
   ],
   providers: [],
   bootstrap: [AppComponent]
