de17da0c5b3b485b8458266e2b701de66e3111a2
acrispin06 <alvarocrispin0604@gmail.com>
2025-05-14 00:14:19 -0500

    feat: implemented module routing on translation

--- src/app/translation/translation-routing.module.ts
+++ src/app/translation/translation-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { LiveTranslateComponent } from './ui/live-translate/live-translate.component';
+//import { TranslationHistoryComponent } from './ui/history/translation-history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: 'translate', component: LiveTranslateComponent },
+  //{ path: 'history',   component: TranslationHistoryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
