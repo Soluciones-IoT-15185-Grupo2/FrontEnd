c4905f08ee02380d3a87c55e062c8702b50945b1
acrispin06 <alvarocrispin0604@gmail.com>
2025-05-13 23:20:32 -0500

    feat: shared module implemented

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+//UI Components
+import { ButtonComponent } from './ui/button/button.component';
+import { TitleCasePipe } from './utils/title-case.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ButtonComponent,
+    TitleCasePipe
+  ],
   imports: [
     CommonModule
   ]
