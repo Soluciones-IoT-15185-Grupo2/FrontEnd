285ff3f5339ccea425139c5b62aab12d1f6a64cf
acrispin06 <alvarocrispin0604@gmail.com>
2025-05-14 01:56:17 -0500

    feat: added routes for translation

--- src/app/translation/translation-routing.module.ts
+++ src/app/translation/translation-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { LiveTranslateComponent } from './ui/live-translate/live-translate.component';
+import { LiveTranslateComponent } from './ui/live-translate/live-translate.component';
 //import { TranslationHistoryComponent } from './ui/history/translation-history.component';
 
 const routes: Routes = [
-  //{ path: 'translate', component: LiveTranslateComponent },
+  { path: 'translate', component: LiveTranslateComponent },
   //{ path: 'history',   component: TranslationHistoryComponent }
 ];
 
