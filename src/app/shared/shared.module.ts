16a8f2865a275350965f8993f3c68e883dd37e7c
acrispin06 <alvarocrispin0604@gmail.com>
2025-05-14 02:04:03 -0500

    feat: adding angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-//UI Components
+// UI Components
 import { ButtonComponent } from './ui/button/button.component';
+
+// Utils (pipes, directives…)
 import { TitleCasePipe } from './utils/title-case.pipe';
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    TitleCasePipe
+    TitleCasePipe,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonComponent,
+    TitleCasePipe,
   ]
 })
 export class SharedModule { }
