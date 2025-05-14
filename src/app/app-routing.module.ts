480ec0468fb4cefdc7374f23fd1ab5ea2f240241
acrispin06 <alvarocrispin0604@gmail.com>
2025-05-14 01:55:43 -0500

    feat: added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'translation',
+    loadChildren: () => import('./translation/translation.module').then(m => m.TranslationModule)
+  },
+  { path: '', redirectTo: 'translation', pathMatch: 'full' },
+  { path: '**', redirectTo: 'translation' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
