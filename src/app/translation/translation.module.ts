df9576b6ba8873e945ab331c863766ee47bae9d5
acrispin06 <alvarocrispin0604@gmail.com>
2025-05-13 22:59:16 -0500

    initial commit

---
+++ src/app/translation/translation.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TranslationRoutingModule } from './translation-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TranslationRoutingModule
+  ]
+})
+export class TranslationModule { }
