9d5573f30b0e1300cb598a1bf46103fd544bbb89
acrispin06 <alvarocrispin0604@gmail.com>
2025-05-14 00:12:07 -0500

    feat: implemented module translation

--- src/app/translation/translation.module.ts
+++ src/app/translation/translation.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
 import { TranslationRoutingModule } from './translation-routing.module';
-
+import { SharedModule } from '../shared/shared.module';
+import { TranslationApiServiceService } from './infraestructure/translation-api.service.service';
+import { TranslationAppServiceService } from './application/translation-app.service.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    TranslationRoutingModule
+    TranslationRoutingModule,
+    HttpClientModule,
+    SharedModule
+  ],
+  providers: [
+    TranslationApiServiceService,
+    TranslationAppServiceService
   ]
 })
 export class TranslationModule { }
