8687a8ed082c4d1555b532e7091886b90bb58da7
Marco Mendez <93227413+Cuak123@users.noreply.github.com>
2025-06-21 18:09:14 -0500

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// think about testing with mockito
 import '@testing-library/jest-dom';
